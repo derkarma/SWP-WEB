55cfde9a7e1cf6f70fe73a7cfce880e8f0923028
jonathan <jonathan.stampfer@gmail.com>
2022-11-22 12:11:20 +0100

    reordered files; 01 javascript task done

---
+++ JAVASCRIPT/JSVariablesAndOperations/script.js
@@ -0,0 +1,43 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+// Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+
+// Berechne folgendes und gib das Eregebnis über console.log aus
+
+// a + b + c
+let result = a + b + c;
+console.log("a + b + c = " + result)
+
+
+// (a + b) / c
+result = (a+b)/c;
+console.log("(a+b)/c) = " + result);
+
+// c -a + b
+result = c -a + b;
+console.log("c - a + b = " + result);
+
+// a + b + c * 3
+result = a + b + c * 3;
+console.log("a + b + c * 3 = " + result);
+
+// (a + b + c)/4
+result = (a + b + c)/4;
+console.log("(a + b + c)/4 = " + result);
+
+// Ändere die Variablen auf folgende Werte
+// a = 3
+a = 3;
+
+// b = a + 3
+b = a + 3;
+
+// c = c -3
+c = c - 3;
+
+// gib die neuen Werte der Variablen aus
+console.log("A hat den Wert: " + a , " B hat den Wert: " + b, " C hat den Wert: " + c)
