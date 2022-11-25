5c45a6ba2d046012e124d354fb522930d6dadc06
jonathan <jonathan.stampfer@gmail.com>
2022-11-25 08:36:01 +0100

    finished task 02 - strings and numbers

---
+++ JAVASCRIPT/JSStringsAndNumbers/stringsandnumbers.js
@@ -0,0 +1,11 @@
+let a = 17.4;
+let b = 3.9;
+
+result = a/b;
+
+console.log("The result of " + a + " divided by " + b + " is " + result);
+
+let firstname = "Jonathan";
+let lastname = " Stampfer";
+
+console.log("Ich heiße " + firstname + lastname)
