0439b807d1c0240f9232ab8ffad33effce275a6b
jonathan <jonathan.stampfer@gmail.com>
2023-01-24 11:54:25 +0100

    finished random number generator

---
+++ JS&HTML/RandomNumberGenerator/randomnumbergenerator.js
@@ -0,0 +1,10 @@
+function generate(){
+let number1 = parseInt(document.getElementById("field1").value);
+let number2 = parseInt(document.getElementById("field2").value);
+
+let amountOfRandomNumbers = parseInt(number2) - parseInt(number1);
+console.log(number1 + " : " + number2);
+
+    let randomNumber = Math.floor(Math.random() * amountOfRandomNumbers) + parseInt(number1) + 1;
+    document.getElementById(result).innerHTML = randomNumber;
+}
