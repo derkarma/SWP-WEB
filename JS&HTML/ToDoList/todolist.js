6614d55d6b21965c9c452b155f6205ac5f82995d
jonathan <jonathan.stampfer@gmail.com>
2023-01-27 09:34:15 +0100

    still working on todolist

---
+++ JS&HTML/ToDoList/todolist.js
@@ -0,0 +1,39 @@
+
+let todos = [
+    {id:1, title:"putzen", isDone:false},
+    {id:2, title:"kochen", isDone:true},
+    {id:3, title:"einkaufen", isDone:false},
+    {id:4, title:"lernen", isDone:false},
+];
+let maxId = 4;
+
+document.getElementById("input").addEventListener("change",function(event){
+    console.log(event.target.value);
+    maxId++;
+    todos.push({id:maxId, title:event.target.value, isDone:false})
+    printTasks();
+    event.target.value = "";
+})
+
+
+
+
+function printTasks(){
+    let html = "";
+    todos.forEach((element) => {
+        html += "<div class='task'>" + element.title + " - " + element.isDone + "<div onClick='setIsDone(" + element.id + ")>Done</div><div>Delete</div></div>";
+    });
+
+    document.getElementById("todos").innerHTML = html;
+
+}
+
+function setIsDone(id){
+    alert("isdone:" + id)
+
+}
+
+printTasks();
+
+
+
