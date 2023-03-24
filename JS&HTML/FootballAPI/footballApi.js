e74b9d104fdd7d5254f42a8ebfde484cf8a8a209
jonathan <jonathan.stampfer@gmail.com>
2023-03-24 09:32:57 +0100

    still working on football api

--- JS&HTML/FootballAPI/footballApi.js
+++ JS&HTML/FootballAPI/footballApi.js
@@ -1,7 +1,19 @@
+console.log("in");
 const loadData = ()=>{
-    fetch("https://api.opnligadb.de/getbltable/bl1/2022").then((result)=>{
+    fetch("https://www.openligadb.de/api/getbltable/bl1/2022").then((result)=>{
         result.json().then((data)=>{
             console.log(data);
+            fillTable(data)
         })
     })
 }
+
+const fillTable = (data)=>{
+    let output = [];
+    data.forEach(element => {
+        output.push("<div>" + element.TeamName + "</div>")
+    });
+    document.getElementById("output").innerHTML = output;
+}
+
+loadData();
